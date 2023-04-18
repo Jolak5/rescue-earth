63313cce77a2f846870e031b2782c71250910483
jolak5 <kayodej4@gmail.com>
2023-04-18 12:59:06 +0100

    fix linters

--- src/components/tests/rendercard.test.js
+++ src/components/tests/rendercard.test.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { cleanup, render } from '@testing-library/react';
 import { Provider } from 'react-redux';
-import RenderCard from '../individual/RenderCard';
+import IndividualCard from '../individual/IndividualCard';
 import store from '../../redux/store';
 
 afterEach(cleanup);
 
 describe('RenderCard', () => {
   it('RenderCard to match Snapshot', () => {
-    const individual = render(<Provider store={store}><RenderCard /></Provider>);
+    const individual = render(<Provider store={store}><IndividualCard /></Provider>);
     expect(individual).toMatchSnapshot();
   });
 });
